22594eabf2240e73d3ab87052150f0e9a304d14c
haasek <Kerstin.Haase@crick.ac.uk>
2017-02-13 13:25:37 +0000

    updated gc correction

--- gcProcessing/createGCcontentFile.R
+++ gcProcessing/createGCcontentFile.R
@@ -1,14 +1,16 @@
 args <- commandArgs(TRUE)
 
+library(plyr)
+
 data<-read.table(args[1])
 name<-args[2]
 
-agg<-aggregate(data$V8,by=list(data$V4,gsub("chr","", data$V1),data$V5), FUN=t)
-agg<-as.data.frame(as.matrix(agg))
+agg<-ddply(data, .(V4, V1, V5), function(z) return(t(z$V8)))
+cols<-ifelse(unique(data$V6)/1000000>=1, paste(unique(data$V6)/1000000, "M", sep=""), unique(data$V6))
+
 rownames(agg)<-agg[,1]
 agg[,1]<-NULL
 
-cols<-ifelse(unique(data$V6)/1000000>=1, paste(unique(data$V6)/1000000, "M", sep=""), unique(data$V6))
 colnames(agg)<-c("Chr","Position",cols)
 agg<-agg[order(agg$Chr, as.numeric(as.character(agg$Position))),]
 write.table(agg,paste(name,".txt",sep=""),quote=F, row.names=T, sep="\t")
