c96ed37c127ce2533960323c55bc8e0787c77a3e
haasek <Kerstin.Haase@crick.ac.uk>
2017-07-10 17:18:35 +0100

    added example pipeline for multi-sample segmentation

--- ExampleData/ASCAT_examplePipeline.R
+++ ExampleData/ASCAT_examplePipeline.R
@@ -27,3 +27,12 @@ gg<-ascat.predictGermlineGenotypes(ascat.bc, platform = "AffySNP6")
 ascat.bc = ascat.aspcf(ascat.bc, ascat.gg=gg)
 ascat.plotSegmentedData(ascat.bc)
 ascat.output = ascat.runAscat(ascat.bc)
+
+#run ASCAT with multi-sample segmentation (when shared breakpoints are expected)
+
+library(ASCAT)
+ascat.bc = ascat.loadData("Tumor_LogR.txt","Tumor_BAF.txt","Germline_LogR.txt","Germline_BAF.txt")
+ascat.plotRawData(ascat.bc)
+ascat.bc = ascat.asmultipcf(ascat.bc)
+ascat.plotSegmentedData(ascat.bc)
+ascat.output = ascat.runAscat(ascat.bc)
