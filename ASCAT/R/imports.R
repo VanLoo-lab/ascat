f4fc04410ca9f7b602a0cbde75fcf255f8ab467c
Tom Lesluyes <t.lesluyes@gmail.com>
2024-05-22 15:27:08 +0200

    Add proper imports

---
+++ ASCAT/R/imports.R
@@ -0,0 +1,13 @@
+#' @importFrom data.table fread
+#' @importFrom doParallel registerDoParallel
+#' @importFrom foreach %dopar% foreach
+#' @importFrom GenomicRanges findOverlaps GRanges pintersect
+#' @importFrom graphics abline axis barplot image par points rect text
+#' @importFrom grDevices adjustcolor colorRampPalette dev.new dev.off pdf png
+#' @importFrom IRanges IRanges
+#' @importFrom RColorBrewer brewer.pal
+#' @importFrom S4Vectors queryHits subjectHits width
+#' @importFrom splines ns
+#' @importFrom stats binom.test cor lm mad median na.omit residuals runif runmed sd
+#' @importFrom utils head read.table write.table
+NULL
